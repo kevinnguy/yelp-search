51833d7e5573a29eb78ce1bef955cadb5b72f329
Kevin Nguy <nguy@macbook.local>
2020-06-10 08:48:05 -0700

    Move App to src directory

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, ScrollView, View, Text} from 'react-native';
+
+import Map from './components/Map';
+import SearchBar from './components/SearchBar';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.map}>
+      <SearchBar />
+      <Map locations={[]} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+  scrollView: {
+    backgroundColor: 'white',
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: 'white',
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'black',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: 'black',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: 'black',
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
