6659666817e2709169001508dfdfcf898a8d5a1b
Kevin Nguy <nguy@macbook.local>
2020-06-10 10:12:10 -0700

    Update Map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 
+import Business from '../types/Business';
+
 interface Coordinate {
   latitude: number;
   longitude: number;
@@ -16,12 +18,16 @@ interface Location {
 }
 
 interface Props {
-  locations: Array<Location>;
+  data: Array<Business>;
 }
 
-const Map: React.FC<Props> = ({locations}) => (
-  <MapView showsUserLocation showsMyLocationButton style={styles.flex}>
-    {locations.map(({coordinate, name}) => (
+const Map: React.FC<Props> = ({data}) => (
+  <MapView
+    showsUserLocation
+    showsMyLocationButton
+    style={styles.flex}
+    maxZoomLevel={13}>
+    {data.map(({coordinate, name}) => (
       <Marker coordinate={coordinate} title={name} />
     ))}
   </MapView>
